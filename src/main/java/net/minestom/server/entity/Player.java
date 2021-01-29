547bc6d30f45ff8210c81762bda13f8a1ba8d629
themode <themode@outlook.fr>
2021-01-29 11:48:47 +0100

    Remove acquisition debug

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -29,7 +29,6 @@ import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.item.metadata.WrittenBookMeta;
 import net.minestom.server.listener.PlayerDiggingListener;
-import net.minestom.server.lock.Acquirable;
 import net.minestom.server.network.ConnectionManager;
 import net.minestom.server.network.ConnectionState;
 import net.minestom.server.network.PlayerProvider;
@@ -337,25 +336,6 @@ public class Player extends LivingEntity implements CommandSender {
         ClientPlayPacket packet;
         while ((packet = packets.poll()) != null) {
             packet.process(this);
-        }
-
-        // TODO REMOVE
-        {
-            //if (getEntityId() == 1)
-                for (Acquirable<Player> acquirable : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
-                    if (acquirable.unsafeUnwrap() == this) {
-                        continue;
-                    }
-
-                    long t = System.nanoTime();
-                    acquirable.acquire(player -> {
-
-                        player.getAcquiredElement().acquire(o -> {
-                            //System.out.println("acquire work "+player.getUsername());
-                        });
-                    });
-                    System.out.println("final " + (System.nanoTime() - t) / 1E6D);
-                }
         }
 
         super.update(time); // Super update (item pickup/fire management)
